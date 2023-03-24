a56becace8e7a9c5da94c57a5682907afb0d1055
YannetGorob <78377338+YannetGorob@users.noreply.github.com>
2023-03-24 16:29:04 +0200

    feat: add weather model

---
+++ lib/features/weather/data/models/weather_model.g.dart
@@ -0,0 +1,149 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_WeatherModel _$$_WeatherModelFromJson(Map<String, dynamic> json) =>
+    _$_WeatherModel(
+      city: json['city'] == null
+          ? null
+          : City.fromJson(json['city'] as Map<String, dynamic>),
+      cod: json['cod'] as String?,
+      message: (json['message'] as num?)?.toDouble(),
+      cnt: json['cnt'] as int?,
+      list: (json['list'] as List<dynamic>?)
+          ?.map((e) => ListElement.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$_WeatherModelToJson(_$_WeatherModel instance) =>
+    <String, dynamic>{
+      'city': instance.city?.toJson(),
+      'cod': instance.cod,
+      'message': instance.message,
+      'cnt': instance.cnt,
+      'list': instance.list?.map((e) => e.toJson()).toList(),
+    };
+
+_$_City _$$_CityFromJson(Map<String, dynamic> json) => _$_City(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      coord: json['coord'] == null
+          ? null
+          : Coord.fromJson(json['coord'] as Map<String, dynamic>),
+      country: json['country'] as String?,
+      population: json['population'] as int?,
+      timezone: json['timezone'] as int?,
+    );
+
+Map<String, dynamic> _$$_CityToJson(_$_City instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'coord': instance.coord,
+      'country': instance.country,
+      'population': instance.population,
+      'timezone': instance.timezone,
+    };
+
+_$_Coord _$$_CoordFromJson(Map<String, dynamic> json) => _$_Coord(
+      lon: json['lon'] as int?,
+      lat: json['lat'] as int?,
+    );
+
+Map<String, dynamic> _$$_CoordToJson(_$_Coord instance) => <String, dynamic>{
+      'lon': instance.lon,
+      'lat': instance.lat,
+    };
+
+_$_ListElement _$$_ListElementFromJson(Map<String, dynamic> json) =>
+    _$_ListElement(
+      dt: json['dt'] as int?,
+      sunrise: json['sunrise'] as int?,
+      sunset: json['sunset'] as int?,
+      temp: json['temp'] == null
+          ? null
+          : Temp.fromJson(json['temp'] as Map<String, dynamic>),
+      feelsLike: json['feels_like'] == null
+          ? null
+          : FeelsLike.fromJson(json['feels_like'] as Map<String, dynamic>),
+      pressure: json['pressure'] as int?,
+      humidity: json['humidity'] as int?,
+      weather: (json['weather'] as List<dynamic>?)
+          ?.map((e) => Weather.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      speed: (json['speed'] as num?)?.toDouble(),
+      deg: json['deg'] as int?,
+      gust: (json['gust'] as num?)?.toDouble(),
+      clouds: json['clouds'] as int?,
+      pop: (json['pop'] as num?)?.toDouble(),
+      rain: (json['rain'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$$_ListElementToJson(_$_ListElement instance) =>
+    <String, dynamic>{
+      'dt': instance.dt,
+      'sunrise': instance.sunrise,
+      'sunset': instance.sunset,
+      'temp': instance.temp,
+      'feels_like': instance.feelsLike,
+      'pressure': instance.pressure,
+      'humidity': instance.humidity,
+      'weather': instance.weather,
+      'speed': instance.speed,
+      'deg': instance.deg,
+      'gust': instance.gust,
+      'clouds': instance.clouds,
+      'pop': instance.pop,
+      'rain': instance.rain,
+    };
+
+_$_FeelsLike _$$_FeelsLikeFromJson(Map<String, dynamic> json) => _$_FeelsLike(
+      day: (json['day'] as num?)?.toDouble(),
+      night: (json['night'] as num?)?.toDouble(),
+      eve: (json['eve'] as num?)?.toDouble(),
+      morn: (json['morn'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$$_FeelsLikeToJson(_$_FeelsLike instance) =>
+    <String, dynamic>{
+      'day': instance.day,
+      'night': instance.night,
+      'eve': instance.eve,
+      'morn': instance.morn,
+    };
+
+_$_Temp _$$_TempFromJson(Map<String, dynamic> json) => _$_Temp(
+      day: (json['day'] as num?)?.toDouble(),
+      min: (json['min'] as num?)?.toDouble(),
+      max: (json['max'] as num?)?.toDouble(),
+      night: (json['night'] as num?)?.toDouble(),
+      eve: (json['eve'] as num?)?.toDouble(),
+      morn: (json['morn'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$$_TempToJson(_$_Temp instance) => <String, dynamic>{
+      'day': instance.day,
+      'min': instance.min,
+      'max': instance.max,
+      'night': instance.night,
+      'eve': instance.eve,
+      'morn': instance.morn,
+    };
+
+_$_Weather _$$_WeatherFromJson(Map<String, dynamic> json) => _$_Weather(
+      id: json['id'] as int?,
+      main: json['main'] as String?,
+      description: json['description'] as String?,
+      icon: json['icon'] as String?,
+    );
+
+Map<String, dynamic> _$$_WeatherToJson(_$_Weather instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'main': instance.main,
+      'description': instance.description,
+      'icon': instance.icon,
+    };
