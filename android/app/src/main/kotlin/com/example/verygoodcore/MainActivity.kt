cba428d372623fd3337b2cee3420d6d61584dd3e
Yehor Pustovoit <63584ee@gmail.com>
2023-03-24 13:19:31 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.weather_app_team
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:weather_app_team/app/app.dart';
+import 'package:weather_app_team/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
